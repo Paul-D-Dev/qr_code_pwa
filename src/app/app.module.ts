a10fa5976289ac28769fd51f3ce06975d0f183cf
Paul DAO <dao.van.paul@gmail.com>
2021-06-06 18:25:45 +0200

    FEAT : call scanner component and qrcode scanner module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,31 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { ZXingScannerModule } from "@zxing/ngx-scanner";
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { ScannerComponent } from './components/scanner/scanner.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScannerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ZXingScannerModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
